09f8c94d7478214e316b72013441ee325c62784e
Samriddhi98 <karmacharyasamriddhi@gmail.com>
2020-08-16 19:15:17 +0545

    snack bar working

--- lib/screens/itemdetail.dart
+++ lib/screens/itemdetail.dart
@@ -40,154 +40,157 @@ class _ItemDetailState extends State<ItemDetail> {
             ),
           ),
         ),
-        body: SingleChildScrollView(
-          reverse: false,
-          child: Padding(
-            padding: EdgeInsets.only(bottom: bottom),
-            child: Container(
-              height: deviceSize.height,
-              child: Column(
-                children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      // The containers in the background
-                      new Column(
-                        children: <Widget>[
-                          new Container(
-                            padding: EdgeInsets.all(5.0),
-                            height: deviceSize.height / 9,
-                            color: Colors.yellow[700],
-                            child: Align(
-                              alignment: Alignment.topRight,
-                            ),
-                          ),
-                          new Container(
-                            height: deviceSize.height / 9,
-                            color: Colors.yellow[700],
-                          )
-                        ],
-                      ),
-                      // The card widget with top padding,
-                      // incase if you wanted bottom padding to work,
-                      // set the `alignment` of container to Alignment.bottomCenter
-                      new Container(
-                        alignment: Alignment.topCenter,
-                        padding: new EdgeInsets.only(right: 20.0, left: 20.0),
-                        child: new Container(
-                          height: 200.0,
-                          width: MediaQuery.of(context).size.width,
-                          color: Colors.red,
-                          child: Image.asset(
-                            loadedFood.image,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Container(
-                    padding: EdgeInsets.only(top: 5.0, left: 10.0, right: 10.0),
-                    height: deviceSize.height / 9,
-                    width: deviceSize.width,
-                    // color: Colors.blue,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Builder(
+                  builder: (context) => SingleChildScrollView(
+            reverse: false,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: bottom),
+              child: Container(
+                height: deviceSize.height,
+                child: Column(
+                  children: <Widget>[
+                    Stack(
                       children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        // The containers in the background
+                        new Column(
                           children: <Widget>[
-                            Text(
-                              loadedFood.title,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
+                            new Container(
+                              padding: EdgeInsets.all(5.0),
+                              height: deviceSize.height / 9,
+                              color: Colors.yellow[700],
+                              child: Align(
+                                alignment: Alignment.topRight,
                               ),
                             ),
-                            Text(
-                              'By: ' "${loadedFood.chefname}",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                            Text(
-                              'From:' "${loadedFood.address}",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.normal,
-                              ),
+                            new Container(
+                              height: deviceSize.height / 9,
+                              color: Colors.yellow[700],
                             )
                           ],
                         ),
-                        Column(
-                          //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              'Rs.' "${loadedFood.price}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                                fontSize: 18.0,
-                              ),
+                        // The card widget with top padding,
+                        // incase if you wanted bottom padding to work,
+                        // set the `alignment` of container to Alignment.bottomCenter
+                        new Container(
+                          alignment: Alignment.topCenter,
+                          padding: new EdgeInsets.only(right: 20.0, left: 20.0),
+                          child: new Container(
+                            height: 200.0,
+                            width: MediaQuery.of(context).size.width,
+                            color: Colors.red,
+                            child: Image.asset(
+                              loadedFood.image,
+                              fit: BoxFit.cover,
                             ),
-                            Icon(
-                              Icons.favorite_border,
-                              size: 35.0,
-                              color: Colors.yellow[700],
-                            ),
-                          ],
-                        ),
+                          ),
+                        )
                       ],
                     ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Container(
-                    height: 40.0,
-                    width: 350.0,
-                    child: GestureDetector(
-                      onTap: () {
-                             cart.addItem(loadedFood.id, loadedFood.price, loadedFood.title,
-                                loadedFood.image);
-                            cart.getTotalPrice();
-                            Scaffold.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Added item to Cart'),
-                              duration: Duration(seconds: 2),
-                              action: SnackBarAction(
-                                  label: 'UNDO',
-                                  textColor: Colors.white,
-                                  onPressed: () {
-                                    cart.removeSingleItem(loadedFood.id);
-                                  }),
-                            ),
-                          );
-                          },
-                                          child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        shadowColor: Colors.black87,
-                        color: Colors.yellow[700],
-                        elevation: 7.0,
-                          child: Center(
-                              child: Text('ADD TO CART',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ))),
-                          
-                        
+                    SizedBox(height: 10.0),
+                    Container(
+                      padding: EdgeInsets.only(top: 5.0, left: 10.0, right: 10.0),
+                      height: deviceSize.height / 9,
+                      width: deviceSize.width,
+                      // color: Colors.blue,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text(
+                                loadedFood.title,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'By: ' "${loadedFood.chefname}",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              Text(
+                                'From:' "${loadedFood.address}",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              )
+                            ],
+                          ),
+                          Column(
+                            //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                'Rs.' "${loadedFood.price}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                              Icon(
+                                Icons.favorite_border,
+                                size: 35.0,
+                                color: Colors.yellow[700],
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Expanded(child: Description(loadedFood.description)),
-                ],
+                    SizedBox(height: 5.0),
+                    Container(
+                      height: 40.0,
+                      width: 350.0,
+                      child: GestureDetector(
+                        onTap: () {
+                               cart.addItem(loadedFood.id, loadedFood.price, loadedFood.title,
+                                  loadedFood.image);
+                              cart.getTotalPrice();
+                              Scaffold.of(context).hideCurrentSnackBar();
+                              Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Added item to Cart'),
+                                duration: Duration(seconds: 2),
+                                action: SnackBarAction(
+                                    label: 'UNDO',
+                                    textColor: Colors.white,
+                                    onPressed: () {
+                                      cart.removeSingleItem(loadedFood.id);
+                                    }),
+                              ),
+                            );
+                            },
+                                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.black87,
+                          color: Colors.yellow[700],
+                          elevation: 7.0,
+                            child: Center(
+                                child: Text('ADD TO CART',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ))),
+                            
+                          
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Expanded(child: Description(loadedFood.description)),
+                  ],
+                ),
               ),
             ),
           ),
